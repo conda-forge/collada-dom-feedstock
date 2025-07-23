0801ec00ac6f51256cd7030ac192d909126b7a5c
Silvio Traversaro <silvio@traversaro.it>
2025-07-23 15:35:32 +0200

    Update build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -3,7 +3,7 @@
 mkdir build
 cd build
 
-cmake ${CMAKE_ARGS} .. \
+cmake ${CMAKE_ARGS} -DCMAKE_POLICY_VERSION_MINIMUM=3.5 .. \
       -DCMAKE_BUILD_TYPE=Release \
       -DCMAKE_PREFIX_PATH=$PREFIX \
       -DCMAKE_INSTALL_PREFIX=$PREFIX \

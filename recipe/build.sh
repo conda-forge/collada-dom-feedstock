9cda14cf0c7b5aa8fec4578701a08f9e633d559a
Silvio Traversaro <silvio@traversaro.it>
2025-10-08 20:56:10 +0200

    Add C++14 standard specification to build script
    Specify C++14 standard for the build process.

--- recipe/build.sh
+++ recipe/build.sh
@@ -6,12 +6,14 @@ rm -rf ./dom/external-libs
 mkdir build
 cd build
 
+# We specify the use of C++14 as std::auto_ptr is used in the public interface, so we can't patch it without an ABI break
 cmake ${CMAKE_ARGS} .. \
       -DCMAKE_BUILD_TYPE=Release \
       -DCMAKE_PREFIX_PATH=$PREFIX \
       -DCMAKE_INSTALL_PREFIX=$PREFIX \
-      -DCMAKE_INSTALL_LIBDIR=lib 
+      -DCMAKE_INSTALL_LIBDIR=lib \
+      -DCMAKE_CXX_STANDARD=14 \
+      -DCMAKE_CXX_STANDARD_REQUIRED=ON
 
 make -j${CPU_COUNT}
 make install
-

e39d0e6db8a43e79fff806e92f3710974c6fec7e
Silvio Traversaro <silvio@traversaro.it>
2026-04-26 16:56:49 +0200

    Set minimum required CMake version to 3.5

--- recipe/build.bat
+++ recipe/build.bat
@@ -13,6 +13,7 @@ cmake ^
     -G "Ninja" ^
     -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
     -DCMAKE_BUILD_TYPE=Release ^
+    -DCMAKE_MINIMUM_REQUIRED_VERSION=3.5 ^
     %SRC_DIR%
 if errorlevel 1 exit 1
 
